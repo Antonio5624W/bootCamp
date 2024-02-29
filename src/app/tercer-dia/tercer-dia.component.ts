b1b6b307eb0f1608ec093c21cbcdabfcd8355121
Antonio5624W <chui.jaso@gmail.com>
2024-02-29 12:24:24 -0700

    todos los componentes que manejamos en el tercer dia

--- src/app/tercer-dia/tercer-dia.component.ts
+++ src/app/tercer-dia/tercer-dia.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { ExampleService } from '../services/example.service';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tercer-dia',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './tercer-dia.component.html',
   styleUrl: './tercer-dia.component.scss'
 })
 export class TercerDiaComponent {
+  name = '';
+  users: any = []
+  constructor(public exampleService: ExampleService) {
+  }
 
+  ngOnInit() {
+    this.exampleService.obtenerlista().subscribe(resp => {
+      console.log(resp);
+      this.users = resp;
+    })
+  }
 }
